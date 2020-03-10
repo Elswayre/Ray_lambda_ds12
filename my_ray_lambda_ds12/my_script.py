291e956aa95494c99e583444c90bd8a409ae13a0
Elswayre <sylver1000@gmail.com>
2020-03-10 14:51:29 -0500

    demonstrate the need to remove code from global scope from module we nwant to input.

--- my_ray_lambda_ds12/my_script.py
+++ my_ray_lambda_ds12/my_script.py
@@ -2,5 +2,11 @@ print('hello world')
 
 import pandas
 
+from my_ray_lambda_ds12.my_mod import enlarge
+
 df = pandas.DataFrame({'state': ['ct', 'co', 'ca', 'tx']})
 print(df.head())
+
+x = 5
+print('number is', x)
+print('enlarged number is', enlarge(x))
